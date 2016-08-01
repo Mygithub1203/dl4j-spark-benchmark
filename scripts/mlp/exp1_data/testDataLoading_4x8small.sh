a8f7fc46a372f11f2b71b3fad7360b7729d54ebd
Alex Black <blacka101@gmail.com>
2016-08-01 14:00:32 +1000

    Add extra experiment launch scripts

--- scripts/mlp/exp1_data/testDataLoading_4x8small.sh
+++ scripts/mlp/exp1_data/testDataLoading_4x8small.sh
@@ -1,5 +1,7 @@
+#testDataLoading_4x8small.sh and #testDataLoading_4x8medium.sh are identical, except for the number of DataSet objects
+#Before running: set the temporary and results directories in the first line below
 JARARGS="-tempPath hdfs:/exp1_data_4x8/temp/ -resultPath hdfs:/exp1_data_4x8/results/ -useSparkLocal false"
-JARARGS="$JARARGS -dataLoadingMethods SparkBinaryFiles StringPath Parallelize CSV SequenceFile -numDataSetObjects 480 6400 -numParams 100000 -dataSize 100 10000"
+JARARGS="$JARARGS -dataLoadingMethods SparkBinaryFiles StringPath Parallelize CSV SequenceFile -numDataSetObjects 480 -numParams 100000 -dataSize 100 10000"
 JARARGS="$JARARGS -miniBatchSizePerWorker 8 -saveUpdater true -repartition Always -repartitionStrategy Balanced"
 JARARGS="$JARARGS -workerPrefetchNumBatches 2"
 SPARKARGS="--class org.deeplearning4j.train.RunTrainingTests --num-executors 4 --executor-cores 8 --executor-memory 10G --driver-memory 10G"
